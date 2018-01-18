41584a91d717fdb2c71c30e9fe31569d1132d042
Merizza Khan <merizza.azwir@gmail.com>
2018-01-18 13:07:48 +0000

    Removed disk storage setup for multer

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,7 @@ const express = require("express"),
       multer = require("multer");
 var router = express.Router();
 
-var storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "./public/images/uploads");
-  },
-  filename: (req, file, cb) => {
-    var extArray = file.mimetype.split("/");
-    var extension = extArray[extArray.length - 1];
-    cb(null, file.fieldname + "-" + Date.now() + "." + extension);
-  }
-});
-
-var upload = multer({storage: storage});
+var upload = multer({ dest: "" });
 
 router.get("/", (req, res) => {
   res.render("index");
